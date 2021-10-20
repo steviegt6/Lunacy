54129cc0fbc0d5c1e9de2b6ea30c99299a61ae8a
Steviegt6 <xxlennygamerxx@gmail.com>
2021-10-20 16:03:34 -0700

    About page additions

--- Patches/resources/app/lunacy.js.patch.c
+++ Patches/resources/app/lunacy.js.patch.c
@@ -4,6 +4,8 @@ const { clipboard } = require("electron");
 
 let texturesEnabled = false;
 
+const lunacyVersion = "0.1.0";
+
 exports.debugClip = function(text) {
     console.log("Coping text to clipboard: " + text);
     clipboard.writeText(text);
@@ -11,4 +13,34 @@ exports.debugClip = function(text) {
 
 exports.areTexturesEnabled = function() {
     return texturesEnabled;
+}
+
+exports.createVersionCard = function(ke) {
+    class LunacyVersionCard extends ke.Component {
+        render() {
+          return ke.createElement(
+            "div",
+            { className: "card" },
+            ke.createElement(
+              "h1",
+              { className: "lunar-text" },
+              ke.createElement("i", { className: "fas fa-code-branch mr-1" }),
+              "LUNACY VERSION"
+            ),
+            ke.createElement("h4", null, "v", lunacyVersion),
+            ke.createElement(
+              "p",
+              null,
+              "A cool little mod for Lunar's Launcher."
+            ),
+            ke.createElement(
+                "p",
+                null,
+                "Made for fun."
+              )
+          );
+        }
+      }
+    
+      return LunacyVersionCard;
 }
