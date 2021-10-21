393dcfed3200476d544e610f2776c6213dd6e036
Charlie <45357714+Metacinnabar@users.noreply.github.com>
2021-10-21 11:27:53 +1100

    Add command checks & fix npm permissions for setup script; Add Rider .idea directory to .gitignore (#1)
    * Add command checks & fix npm permissions
    
    * Delete .idea/.idea.Lunacy/.idea directory
    
    * Add Rider .idea folder
    
    Co-authored-by: GoodPro712 <45357714+GoodPro712@users.noreply.github.com>

--- setup.sh
+++ setup.sh
@@ -1,17 +1,27 @@
-# assuming git is alr installed if you're using bash...
-# todo: safety checks for git & .net like in the batch script
+#!/bin/bash
 
-echo Restoring git submodules
+if ! type "git" > /dev/null; then
+	echo "Please install git before running this script (https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)"
+	exit 1
+fi
 
+echo "Restoring git submodules"
 git submodule update --init --recursive
 
-echo installing jsnice and prettier
-npm i -g jsnice
-npm i -g prettier
+if ! type "npm" > /dev/null; then
+	echo "Please install npm before running this script (https://docs.npmjs.com/downloading-and-installing-node-js-and-npm)"
+	exit 1
+fi
 
-echo ATTENTION: .NET 5.0 is required to build and run these tools, if you do not have .NET 5.0 installed, this will not work
+echo Globally installing jsnice and prettier with npm
+sudo npm i -g jsnice
+sudo npm i -g prettier
 
-# / should be safe for paths, maybe
+if ! type "dotnet" > /dev/null || ! dotnet --list-sdks | grep -q '5.0'; then
+	echo "Please install .NET 5.0 before running this script (https://dotnet.microsoft.com/download/dotnet/5.0)"
+	exit 1
+fi
+
+# should be safe for paths, maybe
 dotnet build Lunacy.Patch/Lunacy.Patch.csproj --configuration Debug
-
 dotnet Lunacy.Patch/bin/Debug/net5.0/Lunacy.Patch.dll
