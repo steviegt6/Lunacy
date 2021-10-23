1dac75bd2178da1f10606343e74882468367a0a5
Steviegt6 <xxlennygamerxx@gmail.com>
2021-10-23 02:00:51 -0700

    Move blog post to website + begin settings stuff...

--- Patches/resources/app/lunacy.js.patch.c
+++ Patches/resources/app/lunacy.js.patch.c
@@ -8,8 +8,10 @@ let texturesEnabled = false;
 
 const lunacyVersion = "0.1.0";
 
+const blogUrl = "https://tomat.dev/assets/lunacy/blog.json";
+
 exports.debugClip = function(text) {
-    console.log("Coping text to clipboard: " + text);
+    console.log("Copying text to clipboard: " + text);
     clipboard.writeText(text);
 };
 
@@ -43,6 +45,66 @@ exports.createVersionCard = function(ke) {
     return LunacyVersionCard;
 };
 
+exports.getSettingsPage = function(ke) {
+    class LunacySettingsPage extends ke.Component {
+        render() {
+            return ke.createElement(
+                "div",
+                { className: "large-container container-fluid" },
+                ke.createElement(
+                    "div",
+                    { className: "row" },
+                    ke.createElement(
+                        "div",
+                        { id: "client-settings", className: "col-12" },
+                        ke.createElement(
+                            "div",
+                            { className: "card" },
+                            ke.createElement(
+                                "div",
+                                { id: "client-settings-title", className: "large-card-title lunar-text" },
+                                ke.createElement(
+                                    "hi",
+                                    null,
+                                    ke.createElement("i", { className: "fas fa-cogs" }),
+                                    "Lunacy Settings"
+                                ),
+                                ke.createElement(
+                                    "h6",
+                                    null,
+                                    "Changes & Tweaks for Lunacy"
+                                )
+                            ),
+                            ke.createElement(
+                                "div",
+                                { id: "client-settings-content", className: "card-body"},
+                                ke.createElement(
+                                    "div",
+                                    null,
+                                    ke.createElement(
+                                        "div", 
+                                        { className: "col-8" },
+                                        ke.createElement(
+                                            "div",
+                                            { className: "col-6 mb-3" },
+                                            ke.createElement(
+                                                AfterLaunchActionSetting_AfterLaunchActionSetting,
+                                                null
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            );
+        }
+    }
+
+    return LunacySettingsPage;
+}
+
 exports.getNewBlogPosts = function(blogPosts) {
     blogPosts.pop();
     blogPosts.unshift(getReservedBlogPost());
@@ -50,15 +112,15 @@ exports.getNewBlogPosts = function(blogPosts) {
 };
 
 function getReservedBlogPost() {
-    let blogPost = new Object();
+    var req = new XMLHttpRequest();
+    req.open("GET", blogUrl, false);
+    req.send(null);
 
-    blogPost.title = "Lunacy Release";
-    blogPost.author = "Uranometry";
-    blogPost.image =
-        "https://cdn.discordapp.com/attachments/660646817612169247/900533605292445806/yyyyy.png";
-    blogPost.excerpt =
-        "Lunacy has released as a sort of alpha/beta! New features are coming, stay tuned. <3";
-    blogPost.link = "https://www.github.com/Steviegt6/Lunacy/";
+    return JSON.parse(req.responseText);
+}
 
-    return blogPost;
+function createTestSettingComponent(ke, self) {
+    class TestSettignComponent extends ke.Component {
+
+    }
 }
