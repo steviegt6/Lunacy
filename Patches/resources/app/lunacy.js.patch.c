141e388c10a3e6a07c92279a51e5f2536b7b434c
Steviegt6 <xxlennygamerxx@gmail.com>
2021-10-23 02:18:50 -0700

    temporary

--- Patches/resources/app/lunacy.js.patch.c
+++ Patches/resources/app/lunacy.js.patch.c
@@ -45,62 +45,97 @@ exports.createVersionCard = function(ke) {
     return LunacyVersionCard;
 };
 
-exports.getSettingsPage = function(ke) {
+exports.getSettingsPage = function(ke, one, two, three, four, five) {
     class LunacySettingsPage extends ke.Component {
         render() {
             return ke.createElement(
+              "div",
+              { className: "large-container container-fluid" },
+              ke.createElement(
                 "div",
-                { className: "large-container container-fluid" },
+                { className: "row" },
                 ke.createElement(
+                  "div",
+                  { id: "client-settings", className: "col-12" },
+                  ke.createElement(
                     "div",
-                    { className: "row" },
+                    { className: "card" },
                     ke.createElement(
+                      "div",
+                      {
+                        id: "client-settings-title",
+                        className: "large-card-title lunar-text",
+                      },
+                      ke.createElement(
+                        "h1",
+                        null,
+                        ke.createElement("i", { className: "fas fa-cogs" }),
+                        "Lunacy Settings"
+                      ),
+                      ke.createElement(
+                        "h6",
+                        null,
+                        "Launcher Tweaks & Visuals"
+                      )
+                    ),
+                    ke.createElement(
+                      "div",
+                      { id: "client-settings-content", className: "card-body" },
+                      ke.createElement(
                         "div",
-                        { id: "client-settings", className: "col-12" },
+                        null,
                         ke.createElement(
+                          "div",
+                          { className: "col-8 left" },
+                          ke.createElement(
                             "div",
-                            { className: "card" },
+                            { className: "col-6 mb-3" },
                             ke.createElement(
-                                "div",
-                                { id: "client-settings-title", className: "large-card-title lunar-text" },
-                                ke.createElement(
-                                    "hi",
-                                    null,
-                                    ke.createElement("i", { className: "fas fa-cogs" }),
-                                    "Lunacy Settings"
-                                ),
-                                ke.createElement(
-                                    "h6",
-                                    null,
-                                    "Changes & Tweaks for Lunacy"
-                                )
-                            ),
-                            ke.createElement(
-                                "div",
-                                { id: "client-settings-content", className: "card-body"},
-                                ke.createElement(
-                                    "div",
-                                    null,
-                                    ke.createElement(
-                                        "div", 
-                                        { className: "col-8" },
-                                        ke.createElement(
-                                            "div",
-                                            { className: "col-6 mb-3" },
-                                            ke.createElement(
-                                                AfterLaunchActionSetting_AfterLaunchActionSetting,
-                                                null
-                                            )
-                                        )
-                                    )
-                                )
+                              one,
+                              null
                             )
+                          ),
+                          ke.createElement(
+                            "div",
+                            { className: "col-6 mb-3" },
+                            ke.createElement(
+                              two,
+                              { minMemoryMb: 512, maxMemoryMb: 1024 }
+                            )
+                          ),
+                          ke.createElement(
+                            "div",
+                            { className: "col-6" },
+                            ke.createElement(
+                              three,
+                              null
+                            )
+                          ),
+                          ke.createElement(
+                            "div",
+                            { className: "col-6" },
+                            ke.createElement(
+                              four,
+                              null
+                            )
+                          )
+                        ),
+                        ke.createElement(
+                          "div",
+                          { className: "col-4" },
+                          ke.createElement(
+                            five,
+                            null
+                          )
                         )
+                      )
                     )
+                  )
                 )
+              )
             );
+          }
         }
-    }
 
     return LunacySettingsPage;
 }
@@ -120,7 +155,48 @@ function getReservedBlogPost() {
 }
 
 function createTestSettingComponent(ke, self) {
-    class TestSettignComponent extends ke.Component {
-
+    class TestSettingComponent extends ke.Component {
+        render() {
+            return ke.createElement(
+                "div",
+                { id: "on-launch-settings", className: "mini-card" },
+                ke.createElement("h1", { className: "lunar-text" },
+                ke.createElement("i", { className: "fas fa-play-circle mr-1" }),
+                "After Launch"
+            ),
+            ke.createElement(
+                "h5",
+                { className: "mini-card-subtitle" },
+                "Select which action your launcher should take on launch",
+            ),
+            ke.createElement(
+                "button",
+                {
+                    type: "button",
+                    role: "group",
+                    classname: "btn lunar-text mb-2 selected-setting"
+                },
+                "Hide Launcher"
+            ),
+            ke.createElement("br", null),
+            ke.createElement(
+                "button",
+                {
+                    type: "button",
+                    "data-toggle": "tooltip",
+                    "data-trigger": "hover",
+                    "data-html": "true",
+                    "data-placement": "right",
+                    title: "",
+                    "data-original-title": "<i class='fas fa-2x fa-info-circle mr-2'></i>After the game is launched your launcher will remain open.",
+                    role: "group",
+                    className: "btn lunar-text mb-2"
+                },
+                "Keep Launcher Open"
+            )
+            );
+        }
     }
+
+    return TestSettingComponent;
 }
